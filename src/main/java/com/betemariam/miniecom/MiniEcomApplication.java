552ad2b3f5198fd7a602f351e5f4b147bb771c96
Betemariam <betemariam.tegie@gmail.com>
2025-05-05 20:44:40 +0300

    Initial commit.

---
+++ src/main/java/com/betemariam/miniecom/MiniEcomApplication.java
@@ -0,0 +1,13 @@
+package com.betemariam.miniecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betemariam/miniecom/MiniEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betemariam.miniecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
